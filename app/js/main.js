5b544129ef50e4e8fdd6ad3700428a449eb5691b
datoshcode <datoshcode@gmail.com>
2021-12-17 17:26:26 +0200

    added Product-slider and Products

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,18 @@
 $(function() {
 
+    $('.rate-star').rateYo({
+    rating: 5, 
+    readOnly: true,
+    starWidth: "12px"
+  });
+
+  $('.product-slider__inner').slick({
+    dots: true,
+    arrows: false,
+    // fade: true,
+    slidesToShow: 4,
+    slidesToScroll: 4
+  });
+
   var mixer = mixitup('.products__inner-box');
-  
-})
+});

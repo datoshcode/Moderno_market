7bb1bbe9b6c54079cf401e7f980194f9b75e82db
datoshcode <datoshcode@gmail.com>
2021-12-22 21:16:25 +0200

    adaptive header abd footer

--- app/js/main.js
+++ app/js/main.js
@@ -15,5 +15,16 @@ $(function() {
     slidesToScroll: 4
   });
 
+// Кнопка меню бургер для мобильной версии
+  $('.menu__btn').on('click', function(){
+    $('.menu__list').slideToggle();
+  });
+
+  // Кнопка "Пользователь"" для мобильной версии
+   $('.header__btn-menu').on('click', function(){
+    $('.header__box').toggleClass('active');
+  });
+  
+// строку об mixitup (рейтинг со звездами) указывай в конце общего js
   var mixer = mixitup('.products__inner-box');
 });

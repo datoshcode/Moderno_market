4397723d63787ce1d562423bf5c4845abd75181f
datoshcode <datoshcode@gmail.com>
2021-12-25 20:58:50 +0200

    added product-one

--- app/js/main.js
+++ app/js/main.js
@@ -15,6 +15,16 @@ $(function() {
     slidesToScroll: 4
   });
 
+  // Tabs
+  $('.product-one__tabs .tab').on('click', function(event) {
+var id = $(this).attr('data-id');
+  $('.product-one__tabs').find('.tab-item').removeClass('active-tab').hide();
+  $('.product-one__tabs .tabs').find('.tab').removeClass('active');
+  $(this).addClass('active');
+  $('#'+id).addClass('active-tab').fadeIn();
+  return false;
+});
+
 // Кнопка меню бургер для мобильной версии
   $('.menu__btn').on('click', function(){
     $('.menu__list').slideToggle();
